9b6e88375b06e8d7d5f08023293fac41dca6a4d9
cucy <292016176@qq.com>
2018-05-16 15:50:52 +0800

    Create stop.sh

---
+++ use_script/stop.sh
@@ -0,0 +1,54 @@
+#! /bin/sh
+PORT=$1
+BASEDIR=`dirname $0`/..
+BASEDIR=`(cd "$BASEDIR"; pwd)`
+PROJECT_NAME="@project.artifactId@"
+MAIN_CLASS="$PROJECT_NAME";
+
+if [ ! -n "$PORT" ]; then  
+	echo $"Usage: $0 {port}"
+	exit $FAIL
+fi
+
+echo "stoping application $PROJECT_NAME......"
+
+# 取进程id
+jcpid=`ps -ef | grep -v "grep" | grep "$MAIN_CLASS" | grep "app.port=$PORT" | sed -n '1P' | awk '{print $2}'`
+
+if [ -z $jcpid ]; then
+	# 没有则说明没有启动
+    echo "$PROJECT_NAME is not started or has been stopped!"
+else
+	curl -X POST http://127.0.0.1:$PORT/shutdown
+	for i in {1..30}
+	do
+		jcpid=`ps -ef | grep -v "grep" | grep "$MAIN_CLASS" | grep "app.port=$PORT" | sed -n '1P' | awk '{print $2}'`
+		if [ -z $jcpid ]; then
+	    	echo "$PROJECT_NAME has been stopped!"
+	    	break
+	    else
+	    	echo "stoping the application .. $i"
+	    	sleep 1
+	    fi
+    done
+    jcpid=`ps -ef | grep -v "grep" | grep "$MAIN_CLASS" | grep "app.port=$PORT" | sed -n '1P' | awk '{print $2}'`
+    if [ $jcpid ]; then
+	    [ -z $jcpid ] || kill -15 $jcpid
+		    for i in {1..30}
+			do
+				jcpid=`ps -ef | grep -v "grep" | grep "$MAIN_CLASS" | grep "app.port=$PORT" | sed -n '1P' | awk '{print $2}'`
+				if [ -z $jcpid ]; then
+			    	echo "$PROJECT_NAME has been stopped!"
+			    	break
+			    else
+			    	echo "stoping the application .. $i"
+			    	sleep 1
+			    fi
+		    done
+	fi
+    jcpid=`ps -ef | grep -v "grep" | grep "$MAIN_CLASS" | grep "app.port=$PORT" | sed -n '1P' | awk '{print $2}'`
+    if [ $jcpid ]; then
+	    [ -z $jcpid ] || kill -9 $jcpid
+	    [ $? -eq 0 ] && echo "Stop $PROJECT_NAME OK!" || echo "Stop $PROJECT_NAME Fail!"
+	fi
+fi
