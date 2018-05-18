5dc3ec6491d996cfb379dc1a4905b0cf226e972f
cucy <292016176@qq.com>
2018-05-18 15:53:03 +0800

    Update auto_upziptomcat.sh

--- use_script/auto_upziptomcat.sh
+++ use_script/auto_upziptomcat.sh
@@ -32,12 +32,12 @@ CURRENT_TIME=$(date +"%F_%H-%M-%S")
 
 
 
- 
+
 # 创建上传目录和解压目录(项目目录)
 if ! [[ -d ${UPLOAD_DIR} || -d  ${HISTORY_DIR} ]]; then
-    echo "创建项目目录${UPLOAD_DIR}   ${HISTORY_DIR}"
-    mkdir -p  ${UPLOAD_DIR} 
-    mkdir -p  ${HISTORY_DIR}
+echo "创建项目目录${UPLOAD_DIR}   ${HISTORY_DIR}"
+mkdir -p  ${UPLOAD_DIR} 
+mkdir -p  ${HISTORY_DIR}
 
 fi
 
@@ -46,62 +46,66 @@ fi
 CHANGE_TO_UPLOAD_DIR=$(cd ${UPLOAD_DIR};pwd)
 HAVE_DATA=$(ls ${CHANGE_TO_UPLOAD_DIR} | wc -l )
 
-if [[ ${HAVE_DATA} == 0 ]]; then
-     echo -e "\033[31m没有上传项目到${UPLOAD_DIR}, 退出\033[0m"     
+    if [[ ${HAVE_DATA} == 0 ]]; then
+    echo -e "\033[31m没有上传项目到${UPLOAD_DIR}, 退出\033[0m"     
     exit 1
-fi
+    fi
 
-if [[ $HAVE_DATA -ne 1 ]]; then
-     echo -e "\033[31m项目目录${UPLOAD_DIR},必须有war包,且只有一个war压缩包,退出\033[0m"
-     exit 1
-fi
+    if [[ $HAVE_DATA -ne 1 ]]; then
+    echo -e "\033[31m项目目录${UPLOAD_DIR},必须有war包,且只有一个war压缩包,退出\033[0m"
+    exit 1
+    fi
 
 
-UPLOAD_WAR_NAME=$(cd ${UPLOAD_DIR};ls)
+    UPLOAD_WAR_NAME=$(cd ${UPLOAD_DIR};ls)
+
+FULL_NAME=$(/usr/bin/basename -- ${UPLOAD_WAR_NAME})
+# EXT_NAME="${filename##*.}"
+FILE_NAME="${FULL_NAME%.*}"
 
 # 将指向目录
-PREPARE_NAME="${HISTORY_DIR}/${CURRENT_TIME}"
+    PREPARE_NAME="${HISTORY_DIR}/${CURRENT_TIME}__${FILE_NAME}"
 
 # 解压项目 
-/usr/bin/unzip -oq  ${UPLOAD_DIR}/${UPLOAD_WAR_NAME} -d ${PREPARE_NAME}
+    /usr/bin/unzip -oq  ${UPLOAD_DIR}/${UPLOAD_WAR_NAME} -d ${PREPARE_NAME}
 
-if [[ $? -ne 0 ]]; then
+    if [[ $? -ne 0 ]]; then
     echo "解压文件失败"
     exit 1
-fi
+    fi
 
 # 停止tomcat
-cd ${TOMCAT_BIN_DIR}; ./shutdown.sh 
+    cd ${TOMCAT_BIN_DIR}; ./shutdown.sh 
 
 # 
-echo "删除原来软链接,重新建立新连接,指向新上传项目"
-if ! [[ -z ${CURRENT_DIR}  ]]; then
+    echo "删除原来软链接,重新建立新连接,指向新上传项目"
+    if ! [[ -z ${CURRENT_DIR}  ]]; then
     echo "111"
-   unlink ${CURRENT_DIR} || echo -e "\033[31m删除 ${CURRENT_DIR} 失败\033[0m"
+    unlink ${CURRENT_DIR} || echo -e "\033[31m删除 ${CURRENT_DIR} 失败\033[0m"
 
-fi
-ln -sv ${PREPARE_NAME}  ${CURRENT_DIR}
+    fi
+    ln -sv ${PREPARE_NAME}  ${CURRENT_DIR}
 
 # 删除上传的文件
 cd ${UPLOAD_DIR} && FILE_LIST=$(ls -l |wc -l )
-if ! [[ -z ${FILE_LIST} ]]; then
-   cd ${UPLOAD_DIR} && rm -rf *
-fi
+    if ! [[ -z ${FILE_LIST} ]]; then
+    cd ${UPLOAD_DIR} && rm -rf *
+    fi
 
 
 # 删除ROOT目录
 
-if [[ -d ${TOMCAT_WEB_DIR}/ROOT ]]; then
-   cd ${TOMCAT_WEB_DIR}; rm  -rf ./ROOT 
-   # 创建新的连接指向
-   ln -sv      ${CURRENT_DIR}        ${TOMCAT_WEB_DIR}/ROOT 
-   echo -e "\033[32m创建新的连接指向:${CURRENT_DIR} -->  ${TOMCAT_WEB_DIR}/ROOT \033[0m"
-fi
+    if [[ -d ${TOMCAT_WEB_DIR}/ROOT ]]; then
+    cd ${TOMCAT_WEB_DIR}; rm  -rf ./ROOT 
+# 创建新的连接指向
+    ln -sv      ${CURRENT_DIR}        ${TOMCAT_WEB_DIR}/ROOT 
+    echo -e "\033[32m创建新的连接指向:${CURRENT_DIR} -->  ${TOMCAT_WEB_DIR}/ROOT \033[0m"
+    fi
 
 
 
 
 # 启动tomcat
-cd ${TOMCAT_BIN_DIR}; ./startup.sh
+    cd ${TOMCAT_BIN_DIR}; ./startup.sh
 
-echo -e "\033[32m启动${TOMCAT_BASE_NAME}成功\033[0m"
+    echo -e "\033[32m启动${TOMCAT_BASE_NAME}成功\033[0m"
